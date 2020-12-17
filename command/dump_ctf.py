b639b4ad1d45d0fd42870fa3acf7365761f92f5d
Luc SORIGNET <luc.sorignet@gmail.com>
2020-12-17 17:12:49 +0100

    spouet

--- command/dump_ctf.py
+++ command/dump_ctf.py
@@ -4,7 +4,12 @@ import base64
 import os
 
 from global_var import *
+
+from .utils import *
 from .decorator import *
+
+
+
 
 
 def get_category_from_name(guild, name):
@@ -13,6 +18,52 @@ def get_category_from_name(guild, name):
             return category
     return None
 
+async def dump_chall(channel,path):
+    chall_name = channel.name
+    flag = "not found"
+    #checking if old format
+    if chall_name.startswith('chall_'):
+        chall_name = chall_name[6:];
+    dump_path = path+"/challenges/"+chall_name+".md"
+    dump_ressource_path = "../attachements/"+chall_name
+
+    f = open(dump_path, "w")
+    f.write("# "+chall_name.upper()+"\n")
+    async for m in channel.history(limit=500, oldest_first=True):
+        content = m.content
+        if content.startswith(g_bot_token+"flag "):
+            content = content[6:]
+            flag = content
+            content = "flag : `"+content+"`"
+        f.write(content+"\n")
+        for e in m.embeds:
+            print(e.type)
+            if(e.type == "image"):
+                f.write("![image]("+e.url+" \"image\")\n")
+            elif e.type == "link":
+                f.write("["+e.title+"]("+e.url+")\n")
+        for a in m.attachments:
+            chall_attachment_path = path+"/attachements/"+chall_name
+            mkPath(chall_attachment_path)
+            download(a.url,chall_attachment_path+"/"+a.filename)
+            if a.filename.endswith(".png") or a.filename.endswith(".jpg") or a.filename.endswith(".gif"):
+                f.write("!["+a.filename+"]("+dump_ressource_path+"/"+a.filename+" \""+a.filename+"\")\n")
+            else:
+                f.write("- Fichier : ["+a.filename+"]("+dump_ressource_path+"/"+a.filename+")\n")
+    f.close()
+    return flag
+
+def add_summary_to_index(dict,path):
+
+    f = open(path, "a")
+    f.write("\n##  WRITE UPS\n\n| Name | Flags |\n| ------------- | :----:|\n")
+    for key, value in dict.items():
+        #if old keys
+        if key.startswith('chall_'):
+            key = key[6:];
+        f.write("|["+key+"](challenges/"+key+".md)|`"+value+"`| \n")
+    f.close()
+
 @error
 async def cmd(message):
     'dump <ctf>'
@@ -20,25 +71,19 @@ async def cmd(message):
     if msg_input:
         ctf =  msg_input.group(1)
         ctf_cat = get_category_from_name(message.guild, ctf)
+        dic = {}
         if ctf_cat != None:
             path = g_dump_dir+"/"+ctf_cat.name.upper()
-            if not os.path.exists(path):
-                os.makedirs(path)
+            mkPath(path+"/challenges")
             for channel in ctf_cat.channels:
-                if(channel.name != "général"):
-                    path = g_dump_dir+"/"+ctf_cat.name.upper()+"/"+channel.name.upper()+".md"
-                    f = open(path, "w")
-                    f.write("# "+channel.name.upper()+"\n")
-                    async for m in channel.history(limit=500, oldest_first=True):
-                        f.write (m.content+"\n")
-                        for e in m.embeds:
-                            print(e.to_dict())
-                            if(e.type == "image"):
-                                f.write("![image]("+e.url+" \"image\")\n")
-                            elif e.type == "link":
-                                f.write("["+e.title+"]("+e.url+")\n")
-                    f.close()
+                if(channel.name != "général" and channel.name != "flags"):
+                    if channel.name == "infos":
+                       await dump_chall(channel, path)
+                    else:
+                        dic[channel.name] = await dump_chall(channel, path)
 
+            os.replace(path+"/challenges/infos.md", path+"/README.md")
+            add_summary_to_index(dic,path+"/README.md")
         msg = "dump ok !"
         await message.channel.send(f'{message.author.mention} {msg}');
     else:
--- command/utils.py
+++ command/utils.py
@@ -1,7 +1,18 @@
 
 from global_var import *
+import os
+import requests
 
 def help_cmd(cmd):
     return f"{g_bot_token}{Command.__dict__[cmd].__doc__}"
 
 
+def mkPath(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+def download(url, filename):
+    f = requests.get(url)
+    file = open(filename, 'wb')
+    file.write(f.content)
+    file.close()
